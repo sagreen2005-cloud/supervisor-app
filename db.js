5052295bac85ea118a7fdbfd52b3ea88a66820ec
sagreen2005-cloud <sagreen2005@gmail.com>
2026-07-01 20:40:39 -0600

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ let db;
 
 function openDatabase() {
   return new Promise((resolve, reject) => {
-    const request = indexedDB.open("SupervisorAppDB", 1);
+    const request = indexedDB.open("SupervisorAppDB", 2);
 
     request.onupgradeneeded = function(event) {
       db = event.target.result;
@@ -44,6 +44,28 @@ function getAllRecords(storeName) {
     const request = store.getAll();
 
     request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+function deleteRecord(storeName, id) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], "readwrite");
+    const store = transaction.objectStore(storeName);
+    const request = store.delete(id);
+
+    request.onsuccess = () => resolve();
+    request.onerror = () => reject(request.error);
+  });
+}
+
+function updateRecord(storeName, record) {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], "readwrite");
+    const store = transaction.objectStore(storeName);
+    const request = store.put(record);
+
+    request.onsuccess = () => resolve();
     request.onerror = () => reject(request.error);
   });
 }
