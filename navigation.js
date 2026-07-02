6b4d82e7b1ee83901455c23a1bf15d87aa30af4c
sagreen2005-cloud <sagreen2005@gmail.com>
2026-07-01 20:33:03 -0600

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,1 +1,24 @@
+function showPage(page){
 
+switch(page){
+
+case "dashboard":
+
+loadDashboard();
+
+break;
+
+case "employees":
+
+loadEmployeesPage();
+
+break;
+
+default:
+
+document.getElementById("content").innerHTML=
+"<h2>Coming Soon</h2>";
+
+}
+
+}
