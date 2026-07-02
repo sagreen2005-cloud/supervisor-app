9849c5141b301f18048ef730ceed1c22edc075a9
sagreen2005-cloud <sagreen2005@gmail.com>
2026-07-01 20:33:17 -0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,29 @@
+function loadDashboard(){
 
+document.getElementById("content").innerHTML=`
+
+<h2>Dashboard</h2>
+
+<p>Welcome Sergeant.</p>
+
+<div class="card">
+
+<h3>Today's Summary</h3>
+
+<ul>
+
+<li>Employees: --</li>
+
+<li>Reviews Due: --</li>
+
+<li>Equipment Checks: --</li>
+
+<li>Tasks: --</li>
+
+</ul>
+
+</div>
+
+`;
+
+}
