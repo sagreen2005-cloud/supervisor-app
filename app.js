e0fa109e8c14d51e72d900272743e806acfcf1f2
sagreen2005-cloud <sagreen2005@gmail.com>
2026-07-17 01:22:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  if (hasAppPassword() && !isAppUnlocked()) {
+    showAppLockScreen();
+  }
+
   await openDatabase();
-
   await applySavedAppAppearance();
-
   showPage("dashboard");
+  addHeaderLockButton();
+
+  if (hasAppPassword() && !isAppUnlocked()) {
+    showAppLockScreen();
+  }
 });
